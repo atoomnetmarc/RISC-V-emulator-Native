12f7295e9a6214e39395f5fbf0eddf19935eb0bc
atoomnetmarc <atoomnetmarc@mail1.atoomnet.net>
2024-03-26 18:40:24 +0000

    Building on Linux works also it seems.

--- lss.py
+++ lss.py
@@ -5,3 +5,9 @@ env.AddPostAction(
 	env.VerboseAction("objdump -h -S \"$BUILD_DIR/${PROGNAME}.exe\" > \"$BUILD_DIR/${PROGNAME}.lss\"",
 	"Creating $BUILD_DIR/${PROGNAME}.lss")
 )
+
+env.AddPostAction(
+	"$BUILD_DIR/${PROGNAME}",
+	env.VerboseAction("objdump -h -S \"$BUILD_DIR/${PROGNAME}\" > \"$BUILD_DIR/${PROGNAME}.lss\"",
+	"Creating $BUILD_DIR/${PROGNAME}.lss")
+)
