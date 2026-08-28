aa73c96075126d8751f8018f0ec08f0d40c7c2e6
atoomnetmarc <atoomnetmarc@mail1.atoomnet.net>
2026-08-28 13:05:53 +0200

    Copy the built program atomically to avoid exec on a half-written binary

--- copy_binaries.py
+++ copy_binaries.py
@@ -4,7 +4,10 @@ from shutil import copy2
 
 def copybinary(*args, **kwargs):
     os.makedirs('binaries', exist_ok=True)
-    # copy2 preserves the executable bit of the built program.
-    copy2(str(kwargs['target'][0]), 'binaries/'+env['PIOENV'])
+    # copy2 preserves the executable bit of the built program. Copy to a
+    # temporary name and rename, so readers never see a half-written binary.
+    tmp = 'binaries/'+env['PIOENV']+'.tmp'
+    copy2(str(kwargs['target'][0]), tmp)
+    os.replace(tmp, 'binaries/'+env['PIOENV'])
 
 env.AddPostAction("$BUILD_DIR/${PROGNAME}", copybinary)
